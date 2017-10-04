73e9a0d4f2ea9576fb635bc9f324d407ef58c5a8
ST8 <st8@ST8-iMac.local>
2017-10-04 10:16:02 +0300

    buttons and labels added timer func created

--- FocusCounter/ViewController.swift
+++ FocusCounter/ViewController.swift
@@ -9,7 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var second = 0
+    var timer = Timer()
 
+    @IBOutlet weak var actionViewOutlet: UIView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var countdownLabel: UILabel!
+    @IBAction func stepperAction(_ sender: UIStepper) {
+        countdownLabel.text! = String(Int(sender.value))
+        print(sender.value)
+        progressBar.progress = 0.0
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Ekrana dokundun")
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunc), userInfo: nil, repeats: true)
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Elini Çektin")
+    }
+    
+    @objc func timerFunc(){
+        print("çalışıyor")
+        if second <= 0 {
+            timer.invalidate()
+
+        }
+        else
+        {
+             second -= 1
+        }
+        print(second)
+     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
